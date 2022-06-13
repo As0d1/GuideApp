f3515fc7617d209ebb816925367bed5854563864
As0d1 <105333413+As0d1@users.noreply.github.com>
2022-06-13 23:38:12 +0900

    DOne

---
+++ app/src/main/java/com/dsu/guideapp/Boysun_bahori_festival.kt
@@ -0,0 +1,11 @@
+package com.dsu.guideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Boysun_bahori_festival : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_boysun_bahori_festival)
+    }
+}
---
+++ app/src/main/java/com/dsu/guideapp/historical_places.kt
@@ -0,0 +1,11 @@
+package com.dsu.guideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class historical_places : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_historical_places)
+    }
+}
