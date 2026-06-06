900ab6a5cb6f6a9188c87e89bb5ebd3f15fd8e91
As0d1 <105333413+As0d1@users.noreply.github.com>
2026-06-06 20:11:13 +0900

    .

---
+++ app/src/main/java/com/dsu/guideapp/hotels_in_tashkent.kt
@@ -0,0 +1,11 @@
+package com.dsu.guideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class hotels_in_tashkent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hotels_in_tashkent)
+    }
+}
---
+++ app/src/main/java/com/dsu/guideapp/samarkand_hotels.kt
@@ -0,0 +1,11 @@
+package com.dsu.guideapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class samarkand_hotels : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_samarkand_hotels)
+    }
+}
